5f05807740d93e86e4eb750c4ad96c86a27fa8a0
ViavaCos <ViavaCos@hotmail.com>
2024-05-28 16:05:48 +0800

    Add model with shadow

--- readme.md
+++ readme.md
@@ -46,3 +46,112 @@ function animate(){
 animate();
 ```
 
+## 导入fbx模型文件
+```javascript
+import * as THREE from 'three'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
+const scene = new THREE.Scene()
+
+const importAirplane = () => {
+  const modelPath = '../static/airplane.fbx';
+  const loader = new FBXLoader()
+
+  loader.load(modelPath, (data) => {
+    scene.add(data)
+  }, (event) => {
+    console.log(event.loaded / event.total * 100 + "% loaded.");
+  }, (err) => {
+    console.log(err);
+  })
+}
+```
+
+## 导入gltf/glb模型文件
+```javascript
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const scene = new THREE.Scene()
+
+const importAirplane = () => {
+  const modelPath = '../static/airplane.glb';
+  const loader = new GLTFLoader()
+
+  loader.load(modelPath, (data) => {
+    // 和fbx不同的是，它的scene在返回数据的第二层
+    data = data.scene;
+
+    data.traverse((child) => {
+      // 为模型的每一个子元素都设置投射阴影
+      child.castShadow = true
+
+      // 解决颜色暗淡无光的问题（似乎仅适用于.glb的文件）
+      if (child.isMesh) {
+        child.material.emissive =  child.material.color;
+        child.material.emissiveMap = child.material.map ;
+        // child.material.shadowSide = THREE.BackSide
+      }
+    })
+
+    scene.add(data)
+  }, (event) => {
+    console.log(event.loaded / event.total * 100 + "% loaded.");
+  }, (err) => {
+    console.log(err);
+  })
+}
+```
+
+
+## 创建一个球体并赋予其阴影和投影
+```javascript
+const createBall = () => {
+  const sphereGeo = new THREE.SphereGeometry(1, 20, 20)
+  const material = new THREE.MeshStandardMaterial()
+  const sphere = new THREE.Mesh(sphereGeo, material)
+
+  // 2.投射阴影
+  sphere.castShadow = true
+  scene.add(sphere)
+
+  // 创建一个平面
+  const geometry = new THREE.PlaneGeometry(50, 50)
+  const planeMaterial = new THREE.MeshStandardMaterial()
+  const plane = new THREE.Mesh(geometry, planeMaterial)
+  plane.position.set(0, -1, 0)
+  plane.rotation.x = -Math.PI / 2
+
+  // 3.接受阴影
+  plane.receiveShadow = true
+  scene.add(plane)
+
+  // 环境光
+  const light = new THREE.AmbientLight(0xffffff, 1)
+  scene.add(light)
+
+  // 聚光灯
+  const spotLight = new THREE.SpotLight(0xffffff, 2)
+  spotLight.position.set(0, 11, 5)
+  // spotLight.position.set(-5, 5, 15)
+  spotLight.decay = 0; // 纵向沿着光照距离的缩减量
+
+  spotLight.angle = Math.PI / 8 // 散射角度
+  spotLight.penumbra = 0.1 // 聚光锥的半影缩减百分比
+  spotLight.distance = 30;
+  spotLight.shadow.radius = 10
+  spotLight.shadow.mapSize.set(256, 256) // 阴影映射的宽高
+  // spotLight.target.position.set(0, 0, 0)
+
+  spotLight.castShadow = true // 4.开启阴影
+  spotLight.shadow.mapSize.width = 4096; // 设置影子的像素大小，越小越模糊
+  spotLight.shadow.mapSize.height = 4096;
+  spotLight.shadow.camera.near = 0.5;
+  spotLight.shadow.camera.far = 100;
+  scene.add(spotLight)
+
+  // 打开聚光灯助手
+  // const spotLightHelper = new THREE.SpotLightHelper(spotLight, 'lightgreen')
+  // scene.add(spotLightHelper)
+}
+```
--- src/shadow.js
+++ src/shadow.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 // 创建场景、相机、渲染器并挂在至浏览器窗口
 const scene = new THREE.Scene()
@@ -15,14 +17,13 @@ renderer.shadowMap.enabled = true
 
 // 创建一个球
 const createBall = () => {
-  const sphereGeo = new THREE.SphereGeometry(1, 20, 20)
-  // const material = new THREE.MeshBasicMaterial()
-  const material = new THREE.MeshStandardMaterial()
-  const sphere = new THREE.Mesh(sphereGeo, material)
+  // const sphereGeo = new THREE.SphereGeometry(1, 20, 20)
+  // const material = new THREE.MeshStandardMaterial()
+  // const sphere = new THREE.Mesh(sphereGeo, material)
 
-  // 2.投射阴影
-  sphere.castShadow = true
-  scene.add(sphere)
+  // // 2.投射阴影
+  // sphere.castShadow = true
+  // scene.add(sphere)
 
   // 创建一个平面
   const geometry = new THREE.PlaneGeometry(50, 50)
@@ -41,8 +42,8 @@ const createBall = () => {
 
   // 聚光灯
   const spotLight = new THREE.SpotLight(0xffffff, 2)
-  // spotLight.position.set(0, 11, 5)
-  spotLight.position.set(-5, 5, 15)
+  spotLight.position.set(0, 11, 5)
+  // spotLight.position.set(-5, 5, 15)
   spotLight.decay = 0; // 纵向沿着光照距离的缩减量
 
   spotLight.angle = Math.PI / 8 // 散射角度
@@ -53,14 +54,51 @@ const createBall = () => {
   // spotLight.target.position.set(0, 0, 0)
 
   spotLight.castShadow = true // 4.开启阴影
-  spotLight.shadow.mapSize.width = 512;
-  spotLight.shadow.mapSize.height = 512;
+  spotLight.shadow.mapSize.width = 4096; // 设置影子的像素大小，越小越模糊
+  spotLight.shadow.mapSize.height = 4096;
   spotLight.shadow.camera.near = 0.5;
   spotLight.shadow.camera.far = 100;
   scene.add(spotLight)
 
-  const spotLightHelper = new THREE.SpotLightHelper(spotLight, 'lightgreen')
-  scene.add(spotLightHelper)
+  // 打开聚光灯助手
+  // const spotLightHelper = new THREE.SpotLightHelper(spotLight, 'lightgreen')
+  // scene.add(spotLightHelper)
+}
+
+// renderer.outputEncoding = THREE.sRGBEncoding;
+
+const importAirplane = () => {
+  // const modelPath = '../static/airplane.fbx';
+  // const loader = new FBXLoader()
+
+  const modelPath = '../static/airplane.glb';
+  const loader = new GLTFLoader()
+
+  loader.load(modelPath, (data) => {
+    // 和fbx不同的是，它的scene在返回数据的第二层
+    data = data.scene;
+    
+    // data.scale.set(0.01, 0.01, 0.01)
+    // data.position.set(0, -1, 0)
+
+    data.traverse((child) => {
+      // 为模型的每一个子元素都设置投射阴影
+      child.castShadow = true
+
+      // 解决颜色暗淡无光的问题（似乎仅适用于.glb的文件）
+      if (child.isMesh) {
+        child.material.emissive =  child.material.color;
+        child.material.emissiveMap = child.material.map ;
+        // child.material.shadowSide = THREE.BackSide
+      }
+    })
+
+    scene.add(data)
+  }, (event) => {
+    console.log(event.loaded / event.total * 100 + "% loaded.");
+  }, (err) => {
+    console.log(err);
+  })
 }
 
 const animate = () => {
@@ -73,5 +111,7 @@ new OrbitControls(camera, renderer.domElement)
 // 设置相机位置
 camera.position.set(0, 0, 50);
 
+
+importAirplane()
 createBall()
 animate();
